f8f651ca15d3308d226807dbc39439acf1e4e72d
misaka10843 <misaka10843@outlook.jp>
2026-03-25 17:35:59 +0800

    chore: Publish Plugins

---
+++ .js/src/plugins/index.js
@@ -0,0 +1,8 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var komga_1 = __importDefault(require("@plugins/multi/komga"));
+var PLUGINS = [komga_1.default];
+exports.default = PLUGINS;
